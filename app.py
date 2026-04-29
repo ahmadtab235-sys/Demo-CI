11fabc2bcfaa8eafc44ec0de0a709b06ec6b791e
Tabish Ahmad <ahmad.tab797@gmail.com>
2026-04-29 18:17:46 +0530

    4th run

--- app.py
+++ app.py
@@ -14,6 +14,4 @@ if __name__ == "__main__":
     result1 = add(4, 5)
     result2 = sub(4, 1)
     result3 = multiply(4, 5)
-    
-    
-    print(result1, result2)
+    print(result1, result2, result3)
